a7f1a63a77cf1bcb18e2f57b8213ad0936d66071
Great Kali <killcode@localhost.localdomain>
2019-07-22 20:02:40 +0100

    run until user input exit with node findip run once when using -ip tag

--- findip.js
+++ findip.js
@@ -1,16 +1,16 @@
 const redis = require("redis");
 const fetch = require('node-fetch');
-const rd = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+
 const client = redis.createClient(6379);
 client.on("error", (err) => {
     console.log("Something went wrong", err);
     throw err;
 });
+const ask=()=>{
+    process.stdout.write("Enter any IP address > ");
+}
 
-const render = ({ info, ip }) => {
+const render = ({ info, ip,loop }) => {
     let ipinfo = JSON.parse(info);
     console.log("\n", "\n")
     console.log(`IP address: ${ip}`, "\n")
@@ -19,15 +19,21 @@ const render = ({ info, ip }) => {
     console.log("State/Region name:", ipinfo.region_name, "\n")
     console.log("Latitude:", ipinfo.latitude, "\n")
     console.log("Longitude:", ipinfo.longitude, "\n")
+    if(loop){
+        ask()
+    }else{
+        process.exit();
+    }
 }
 const getarg = (flag) => {
     let i = process.argv.indexOf(flag);
     return i === -1 ? null : process.argv[i + 1];
 }
-const findip = (ip) => {
+
+const findip = (ip,loop=false) => {
     client.get(ip, (err, ipinfo) => {
         if (ipinfo) {
-            render({ ip: ip, info: ipinfo });
+            render({ ip: ip, info: ipinfo,loop:loop });
         } else {
             let ipstack = "http://api.ipstack.com/" + ip.trim() + "?access_key=ae4854a7fe49882068f3232d261fbb6a&fields=continent_name,country_name,region_name,latitude,longitude";
 
@@ -37,7 +43,7 @@ const findip = (ip) => {
                     // Save the  response in Redis db
                     client.set(ip, JSON.stringify(ipinfo));
                     //resturn api response to client
-                    render({ ip: ip, info: JSON.stringify(ipinfo) });
+                    render({ ip: ip, info: JSON.stringify(ipinfo),loop:loop});
                 }).catch((er) => {
                     console.log("error:", er);
                 })
@@ -45,12 +51,16 @@ const findip = (ip) => {
     })
 }
 
+
 let iparg = getarg("-ip");
 if (iparg) {
     findip(iparg)
 } else {
-    rd.question(`Enter any IP address >`, (ip) => {
-        findip(ip);
-        rd.close();
+    ask();
+    process.stdin.on("data",(ip) => {
+        findip(ip.toString().trim(),true);
+        if(ip.toString().trim().toLowerCase() ==="exit"){
+            process.exit();
+        }
     })
 }
