b7ab689b9ab7d6faa9dd6347fb1ae98e405b4549
Great Kali <killcode@localhost.localdomain>
2019-07-22 17:42:16 +0100

    get ip with -ip flag

--- findip.js
+++ findip.js
@@ -11,7 +11,7 @@ client.on("error", (err) => {
 });
 
 const render = ({ info, ip }) => {
-    let ipinfo=JSON.parse(info);
+    let ipinfo = JSON.parse(info);
     console.log("\n", "\n")
     console.log(`IP address: ${ip}`, "\n")
     console.log("Continent name:", ipinfo.continent_name, "\n")
@@ -20,27 +20,38 @@ const render = ({ info, ip }) => {
     console.log("Latitude:", ipinfo.latitude, "\n")
     console.log("Longitude:", ipinfo.longitude, "\n")
 }
+const getarg = (flag) => {
+    let i = process.argv.indexOf(flag);
+    return i === -1 ? null : process.argv[i + 1];
+}
+const findip = (ip) => {
+    client.get(ip, (err, ipinfo) => {
+        if (ipinfo) {
+            render({ ip: ip, info: ipinfo });
+        } else {
+            let ipstack = "http://api.ipstack.com/" + ip.trim() + "?access_key=ae4854a7fe49882068f3232d261fbb6a&fields=continent_name,country_name,region_name,latitude,longitude";
 
-client.on('connect', () => {
+            fetch(ipstack)
+                .then(res => res.json())
+                .then((ipinfo) => {
+                    // Save the  response in Redis db
+                    client.set(ip, JSON.stringify(ipinfo));
+                    //resturn api response to client
+                    render({ ip: ip, info: ipinfo });
+                }).catch((er) => {
+                    console.log("error:", er);
+                })
+        }
+    })
+}
+
+let iparg = getarg("-ip");
+if (iparg) {
+    findip(iparg)
+} else {
     rd.question(`Enter any IP address`, (ip) => {
-        client.get(ip, (err, ipinfo) => {
-            if (ipinfo) {
-                render({ ip: ip, info: ipinfo });
-            } else {
-                let ipstack = "http://api.ipstack.com/" + ip.trim() + "?access_key=ae4854a7fe49882068f3232d261fbb6a&fields=continent_name,country_name,region_name,latitude,longitude";
-
-                fetch(ipstack)
-                    .then(res => res.json())
-                    .then((ipinfo) => {
-                        // Save the  response in Redis db
-                        client.set(ip, JSON.stringify(ipinfo));
-                        //resturn api response to client
-                        render({ ip: ip, info: ipinfo });
-                    }).catch((er) => {
-                        console.log("error:", er);
-                    })
-            }
-        })
+        console.log(ip);
+        findip(ip);
         rd.close();
     })
-})
+}
